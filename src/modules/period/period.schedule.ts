597ecbafa37786dc3785b4353e95847049c8c34a
@lanle98 <lekienlan98@gmail.com>
2024-02-27 23:05:43 +0700

    feat: run overdue cron job

--- src/modules/period/period.schedule.ts
+++ src/modules/period/period.schedule.ts
@@ -8,58 +8,69 @@ import { periodService } from '.';
 
 const scheduledJobs: Record<string, CronJob> = {};
 
-const scheduleTaskForPeriod = (period: periods) => {
-  // Convert period.end_date to a cron pattern
-  // Example: Assuming you want to run the job every day at the time specified by end_date
-  const date = new Date(period.end_date);
-  const cronPattern = `${date.getMinutes()} ${date.getHours()} * * *`;
-
-  const job = new CronJob(
-    cronPattern,
-    async () => {
-      periodService.update(period.id, {
-        status: 'completed'
-      });
-      if (!period.repeat) {
-        period.members.forEach(async (member) => {
-          const memberExpense = await transactionService.findMany({
-            user_id: member,
-            type: 'expense',
-            period_id: period.id
-          });
-          transactionService.create({
-            type: 'income',
-            amount: period.budget - (memberExpense.total_amount || 0),
-            user_id: member
-          });
-        });
-        return;
-      }
-
-      const diff = moment(period.end_date).diff(
-        moment(period.start_date),
-        'minute'
-      );
-      const newEndDate = moment(period.end_date)
-        .add(diff, 'minute')
-        ?.toISOString();
-
-      const expenseTotal = await transactionService.findMany({
+const runPeriodJob = async (period: periods) => {
+  periodService.update(period.id, {
+    status: 'completed'
+  });
+  if (!period.repeat) {
+    period.members.forEach(async (member) => {
+      const memberExpense = await transactionService.findMany({
+        user_id: member,
         type: 'expense',
         period_id: period.id
       });
-
-      await periodService.create({
-        members: period?.members,
-        budget: period.budget - (expenseTotal?.total_amount || 0),
-        pig_id: period?.pig_id,
-        repeat: true,
-        expense: 0,
-        id: undefined,
-        status: 'running',
-        start_date: period.end_date,
-        end_date: newEndDate
+      transactionService.create({
+        type: 'income',
+        amount: period.budget - (memberExpense.total_amount || 0),
+        user_id: member
       });
+    });
+    return;
+  }
+
+  const diff = moment(period.end_date).diff(
+    moment(period.start_date),
+    'minute'
+  );
+  const newEndDate = moment(period.end_date).add(diff, 'minute')?.toISOString();
+
+  const expenseTotal = await transactionService.findMany({
+    type: 'expense',
+    period_id: period.id
+  });
+
+  await periodService.create({
+    members: period?.members,
+    budget: period.budget - (expenseTotal?.total_amount || 0),
+    pig_id: period?.pig_id,
+    repeat: true,
+    expense: 0,
+    id: undefined,
+    status: 'running',
+    start_date: period.end_date,
+    end_date: newEndDate
+  });
+};
+
+const scheduleTaskForPeriod = (period: periods) => {
+  // Convert period.end_date to a cron pattern
+  // Example: Assuming you want to run the job every day at the time specified by end_date
+  const endDate = new Date(period.end_date);
+  const currentDate = new Date();
+
+  // Check if the end date has already passed
+  if (endDate <= currentDate) {
+    // Run the job immediately
+    console.log('Running overdue cron job for period id: ', period?.id);
+    runPeriodJob(period);
+    return;
+  }
+
+  const cronPattern = `${endDate.getMinutes()} ${endDate.getHours()} * * *`;
+  const job = new CronJob(
+    cronPattern,
+    async () => {
+      runPeriodJob(period);
     },
     null,
     true
